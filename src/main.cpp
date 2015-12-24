f5a6e47feee96f4f80416fc277c31df7072b162f
Richard Teammco <teammcr192@gmail.com>
2015-12-24 15:29:34 -0500

    Added OpenCV to the compile.
    Test code in main to make sure it works. It does!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 using namespace std;
 
 
@@ -10,4 +13,14 @@ int main(int argc, char **argv) {
   for (const int i : x) {
     cout << i << endl;
   }
+
+  cv::Mat image = cv::imread("test/cat.jpg", CV_LOAD_IMAGE_COLOR);
+  if (!image.data) {
+    cout << "Failed to load image." << endl;
+    return -1;
+  }
+  cv::namedWindow("Test", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Test", image);
+  cv::waitKey(0);
+  return 0;
 }
