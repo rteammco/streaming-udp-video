5cf3d0dd6100e2272d943616d05f2317d50ad898
Richard Teammco <teammcr192@gmail.com>
2015-12-26 13:43:37 -0500

    Added port processing to sender main as well.
    Also added comments to sender/main.cpp.

--- src/sender/main.cpp
+++ src/sender/main.cpp
@@ -1,3 +1,9 @@
+// This program attempts to gather video frames from a webcam or connected
+// camera, encodes them into a compressed format (JPEG), and sends them over UDP
+// to a receiver of the specified port number and address. The frames should be
+// decodable on the other side and reconstructed into images.
+
+#include "process_port_param.h"
 #include "sender/sender_socket.h"
 #include "sender/video_reader.h"
 
@@ -6,11 +12,14 @@ using udp_video_sender::SenderSocket;
 using udp_video_sender::VideoReader;
 
 int main(int argc, char **argv) {
-  //VideoReader video_reader(true, 75, 1.0);
-  //while (true) {
-  //  video_reader.GetFrameFromCamera();
-  //}
-  SenderSocket socket("127.0.0.1", 8080);
-  socket.SendPacket({'a', 'b', 'c'});
+  const int port = udp_video::ProcessPortParam(argc, argv);
+  if (port < 0) {
+    return -1;
+  }
+  VideoReader video_reader(true, 75, 1.0);
+  SenderSocket socket("127.0.0.1", port);  // TODO: address parameter!
+  while (true) {
+    video_reader.GetFrameFromCamera();
+  }
   return 0;
 }
