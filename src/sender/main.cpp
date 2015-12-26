9cbf68877cb9ed26072e1589b8a6a31330d9e6d4
Richard Teammco <teammcr192@gmail.com>
2015-12-26 01:38:37 -0500

    Updated sender to be able to send real data buffers.
    Tested with both sender and receiver, and they can communicate.

--- src/sender/main.cpp
+++ src/sender/main.cpp
@@ -1,12 +1,16 @@
+#include "sender/sender_socket.h"
 #include "sender/video_reader.h"
 
 
+using udp_video_sender::SenderSocket;
 using udp_video_sender::VideoReader;
 
 int main(int argc, char **argv) {
-  VideoReader video_reader(true, 75, 1.0);
-  while (true) {
-    video_reader.GetFrameFromCamera();
-  }
+  //VideoReader video_reader(true, 75, 1.0);
+  //while (true) {
+  //  video_reader.GetFrameFromCamera();
+  //}
+  SenderSocket socket("127.0.0.1", 8080);
+  socket.SendPacket({'a', 'b', 'c'});
   return 0;
 }
--- src/sender/sender_socket.h
+++ src/sender/sender_socket.h
@@ -3,11 +3,29 @@
 #ifndef SENDER_SENDER_SOCKET_H
 #define SENDER_SENDER_SOCKET_H
 
+#include <arpa/inet.h>
+#include <string>
+#include <vector>
+
 
 namespace udp_video_sender {
 
 class SenderSocket {
+public:
+  SenderSocket(const std::string &receiver_ip, const int receiver_port);
+  
+  // TODO: add destructor to clear the socket
+  // close(fd);
 
+  void SendPacket(const std::vector<unsigned char> &data) const;
+
+private:
+    // The socket identifier (handle).
+    int socket_handle_;
+
+    // The struct that contains the receiver's address and port. This is set up
+    // in the constructor.
+    sockaddr_in receiver_addr_;
 };  // SenderSocket
 
 };  // namespace udp_video_sender
